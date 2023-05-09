c00a778809fa39f8e99f34c579ad94c73ae2a462
Princewill Olaiya <princewillolaiya@gmail.com>
2023-05-09 18:16:20 +0100

    switching works

--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module github.com/just-nibble/wallp-switch
 
 go 1.20
-
-require (
-	github.com/godbus/dbus v4.1.0+incompatible // indirect
-	github.com/mattn/go-isatty v0.0.10 // indirect
-	github.com/mattn/go-tty v0.0.5 // indirect
-	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
-)
--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
-
-	"github.com/mattn/go-tty"
+	"os/user"
+	"strconv"
+	"strings"
 )
 
-func SwitchWallpaper() (msg string, err error) {
-	cmd := exec.Command("plasma-apply-wallpaperimage", "/home/princewillo/Pictures/wallpaper/samurai_strike.jpg")
+var wallpapers []string = []string{}
+
+var usr, _ = user.Current()
+var dir string = usr.HomeDir
+
+var folder string = dir + "/Pictures/wallpaper/"
+var config_file string = dir + "/current_wallpaper.txt"
+
+func SwitchWallpaper(wallpaper int64) (msg string, err error) {
+	var wallpaper_path string = folder + wallpapers[wallpaper]
+	cmd := exec.Command("plasma-apply-wallpaperimage", wallpaper_path)
 
 	var out bytes.Buffer
 	cmd.Stdout = &out
@@ -22,25 +34,60 @@ func SwitchWallpaper() (msg string, err error) {
 	return out.String(), nil
 }
 
-func main() {
-	var key_combo []string = make([]string, 5)
-	tty, err := tty.Open()
+func set_wallpaper(data int64) {
+	f, err := os.Create(config_file)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer tty.Close()
 
-	for {
-		r, err := tty.ReadRune()
-		if err != nil {
-			log.Fatal(err)
-		}
-		key_combo = append(key_combo, string(r))
-		if len(key_combo) >= 4 {
-			if key_combo[0] == "[" && key_combo[1] == "6" && key_combo[2] == "~" && key_combo[3] == "a" {
-				SwitchWallpaper()
-			}
-			key_combo = nil
-		}
+	defer f.Close()
+
+	_, err2 := f.WriteString(strconv.FormatInt(data, 10))
+
+	if err2 != nil {
+		log.Fatal(err2)
 	}
+
+}
+
+func get_current_wallpaper() int64 {
+	content, err := os.ReadFile(config_file)
+	var current string = strings.TrimSuffix(string(content), "\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	current_wallpaper, err := strconv.ParseInt(current, 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return current_wallpaper
+}
+
+func get_wallpapers() {
+	file, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	names, err := file.Readdirnames(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wallpapers = names
+}
+
+func main() {
+	get_wallpapers()
+	var current_wallpaper int64 = get_current_wallpaper()
+	fmt.Println(SwitchWallpaper(current_wallpaper))
+	if current_wallpaper <= int64(len(wallpapers)-1) {
+		current_wallpaper++
+	} else {
+		current_wallpaper = 0
+	}
+	set_wallpaper(current_wallpaper)
 }
